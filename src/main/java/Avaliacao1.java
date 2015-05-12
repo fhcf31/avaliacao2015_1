3eb2361f006f6f6bba5b83e708c1079a781fc703
a1260766 <a1260766@localhost>
2015-05-12 12:03:05 -0300

    segundo commit

--- src/main/java/Avaliacao1.java
+++ src/main/java/Avaliacao1.java
@@ -6,12 +6,28 @@
  * Primeira avaliação parcial 2014/2.
  * @author 
  */
+import utfpr.ct.dainf.if62c.avaliacao.Complexo;
+
 public class Avaliacao1 {
 
     public static void main(String[] args) {
         // implementar main
+        //raizes da equação primeira equação
+        
+        System.out.println();
+        
+        
     }
     
     // implementar raizesEquacao(Complexo, Complexo, Complexo)
+    public static Complexo[] raizesEquacao(Complexo a,Complexo b,Complexo c){
+        
+        Complexo[] raizes = new Complexo[2];
+        
+        raizes[0] = b.prod(-1).soma(b.prod(b).sub(a.prod(4).prod(c)).sqrt()[0].div(a.prod(2)));
+        raizes[1] = b.prod(-1).soma(b.prod(b).sub(a.prod(4).prod(c)).sqrt()[1].div(a.prod(2)));
+        
+        return raizes;
+    }
     
 }
